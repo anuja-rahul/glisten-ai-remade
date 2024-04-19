f823a726c1d5df9efa589c3ba7339d50c8ae0441
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-19 21:11:22 +0530

    Integrations section animations finished.

--- src/slices/Integrations/index.tsx
+++ src/slices/Integrations/index.tsx
@@ -7,18 +7,9 @@ import {
 } from "@prismicio/react";
 import StarBackground from "./StarBackground";
 import Image from "next/image";
-import {
-  FaDigitalOcean,
-  FaCloudflare,
-  FaNpm,
-  FaGithub,
-  FaFigma,
-  FaFly,
-} from "react-icons/fa6";
-import StylizedLogoMark from "./StylizedLogoMark";
 import background from "./background.jpg";
 import React from "react";
-import clsx from "clsx";
+import AnimatedContent from "./AnimatedContent";
 
 /**
  * Props for `Integrations`.
@@ -29,14 +20,7 @@ export type IntegrationsProps = SliceComponentProps<Content.IntegrationsSlice>;
  * Component for "Integrations" Slices.
  */
 const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
-  const icons = {
-    digitalocean: <FaDigitalOcean />,
-    cloudflare: <FaCloudflare />,
-    npm: <FaNpm />,
-    github: <FaGithub />,
-    figma: <FaFigma />,
-    fly: <FaFly />,
-  };
+ 
 
   return (
     <Bounded
@@ -60,35 +44,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
           <PrismicRichText field={slice.primary.body} />
         </div>
 
-        <div className="mt-20 flex flex-col items-center md:flex-row">
-          {slice.items.map((item, index) => (
-            <React.Fragment key={index}>
-              {index === Math.floor(slice.items.length / 2) && (
-                <>
-                  <StylizedLogoMark />
-                  <div className="signal-line rotate-180 bg-gradient-to-t" />
-                </>
-              )}
-              <div
-                className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full 
-              border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 
-              md:text-4xl lg:text-5xl"
-              >
-                {item.icon && icons[item.icon]}
-              </div>
-              {index !== slice.items.length - 1 && (
-                <div
-                  className={clsx(
-                    "signal-line",
-                    index >= Math.floor(slice.items.length / 2)
-                      ? "rotate-180"
-                      : "rotate-0",
-                  )}
-                />
-              )}
-            </React.Fragment>
-          ))}
-        </div>
+        <AnimatedContent slice={slice} />
       </div>
     </Bounded>
   );
